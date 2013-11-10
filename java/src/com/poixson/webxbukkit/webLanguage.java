8928f8587160b79021b039358e2d8bec2494b319
Pete Lorenzo <lorenzo@poixson.com>
2013-11-10 18:40:06 -0500

    added webLanguage class containing a default message

---
+++ java/src/com/poixson/webxbukkit/webLanguage.java
@@ -0,0 +1,15 @@
+package com.poixson.webxbukkit;
+
+import com.poixson.commonjava.Language.xLanguage;
+
+
+public class webLanguage extends xLanguage {
+
+
+	@Override
+	protected void defaults() {
+		addDefault("name",   "Message");
+	}
+
+
+}
