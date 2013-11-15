0d352b430cfcb0b53ee40f744272c153fa48cef8
Pete Lorenzo <lorenzo@poixson.com>
2013-11-15 17:31:09 -0500

    finished LinkManager class, using new handler classes

--- java/src/com/poixson/webxbukkit/webLink/LinkManager.java
+++ java/src/com/poixson/webxbukkit/webLink/LinkManager.java
@@ -3,16 +3,19 @@ package com.poixson.webxbukkit.webLink;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import com.poixson.commonjava.Utils.utilsString;
 import com.poixson.commonjava.Utils.xTime;
 import com.poixson.commonjava.pxdb.dbQuery;
 import com.poixson.webxbukkit.WebAPI;
-import com.poixson.webxbukkit.webLink.handlers.economyFactory;
-import com.poixson.webxbukkit.webLink.handlers.inventoryFactory;
-import com.poixson.webxbukkit.webLink.handlers.permissionsFactory;
+import com.poixson.webxbukkit.webLink.handlers.economyHandler;
+import com.poixson.webxbukkit.webLink.handlers.inventoryHandler;
+import com.poixson.webxbukkit.webLink.handlers.permsHandler;
+import com.poixson.webxbukkit.webLink.handlers.worldguardHandler;
 
 
 public class LinkManager {
@@ -22,25 +25,26 @@ public class LinkManager {
 	}
 
 	// manager instances
-	private static final HashMap<String, LinkManager> instances = new HashMap<String, LinkManager>();
-	private static final HashMap<String, linkFactory> factories = new HashMap<String, linkFactory>();
-	private final HashMap<String, linkHandler> handlers = new HashMap<String, linkHandler>();
+	private static final Map<String, LinkManager> instances  = new HashMap<String, LinkManager>();
+	private static final Map<String, ActionHandler> handlers = new HashMap<String, ActionHandler>();
+
 	// db connection key
 	private final String dbKey;
 
 	// update task
-	private static volatile BukkitRunnable task = null;
-	private static volatile Boolean running = false;
+	private static volatile UpdateTask task = null;
+	private static volatile Boolean active = false;
 
 
 	// get manager instance (per db key)
 	public static LinkManager get(String dbKey) {
 		synchronized(instances) {
-			// register default factories
 			if(instances.isEmpty()) {
-				new economyFactory();
-				new inventoryFactory();
-				new permissionsFactory();
+				// register default handlers
+				register(new economyHandler());
+				register(new inventoryHandler());
+				register(new permsHandler());
+				register(new worldguardHandler());
 			}
 			// use existing manager
 			if(instances.containsKey(dbKey))
@@ -53,147 +57,150 @@ public class LinkManager {
 	}
 	// new manager instance
 	private LinkManager(String dbKey) {
+		if(dbKey == null || dbKey.isEmpty()) throw new NullPointerException("dbKey cannot be null");
 		this.dbKey = dbKey;
 	}
 
 
-	// get action handler
-	public linkHandler getHandler(String handlerName) {
+	// register action handler/listener
+	public static void register(ActionHandler handler) {
 		synchronized(handlers) {
-			// action already exists
-			if(handlers.containsKey(handlerName))
-				return handlers.get(handlerName);
-			synchronized(factories) {
-				// new action from factory
-				if(factories.containsKey(handlerName)) {
-					linkHandler action = factories.get(handlerName).newActionHandler(dbKey);
-					handlers.put(handlerName, action);
-					System.out.println("Registered new action handler: "+handlerName+" dbKey: "+dbKey);
-					return action;
+			String name = handler.getHandlerName();
+			if(!handlers.containsKey(name)) {
+				handlers.put(name, handler);
+				Bukkit.getPluginManager().registerEvents(handler, WebAPI.get());
+			}
+		}
+	}
+
+
+	// start task
+	public void start() {
+		if(task != null) return;
+		synchronized(active) {
+			if(task != null) return;
+			task = new UpdateTask(this);
+			task.runTaskTimerAsynchronously(
+				WebAPI.get(),
+				xTime.get("5s").getTicks(),
+				xTime.get("3s").getTicks()
+			);
+		}
+	}
+
+
+	// update task
+	private class UpdateTask extends BukkitRunnable {
+
+		private final LinkManager manager;
+		public UpdateTask(LinkManager manager) {
+			super();
+			this.manager = manager;
+		}
+
+		@Override
+		public void run() {
+			synchronized(active) {
+				if(active) {
+					System.out.println("WebLink update task still running..");
+					return;
 				}
+				active = true;
 			}
+			manager.triggerUpdates();
+			manager.triggerActions();
+			active = false;
 		}
-		// action or factory not found
-		return null;
+
+	}
+	// fire event in main thread
+	private class ActionTask extends BukkitRunnable {
+
+		private final ActionEvent event;
+		public ActionTask(ActionEvent event) {
+			this.event = event;
+		}
+
+		@Override
+		public void run() {
+			Bukkit.getPluginManager().callEvent(event);
+		}
+
 	}
 
 
-	// register handler factory
-	public static void register(linkFactory factory) {
-		if(factory == null) throw new NullPointerException("factory cannot be null");
-		synchronized(factories) {
-			String name = factory.getHandlerName();
-			if(factories.containsKey(name)) {
-				linkFactory existing = factories.get(name);
-				if(factory != existing) throw new IllegalArgumentException("Handler factory already registered for: "+name);
-				// object already registered, nothing to do
-				return;
-			}
-			// register new factory
-			factories.put(name, factory);
-			System.out.println("Registered new handler factory: "+name);
-		}
-	}
-
-
-	public static void start() {
-		synchronized(instances) {
-			if(task == null) {
-				task = new BukkitRunnable() {
-					@Override
-					public void run() {
-						runUpdates();
-					}
-				};
-				task.runTaskTimerAsynchronously(
-					WebAPI.get(),
-					xTime.get("5s").getTicks(),
-					xTime.get("3s").getTicks()
-				);
-			}
-		}
-	}
-	private static void runUpdates() {
-		if(running) return;
-		synchronized(running) {
-			if(running) return;
-			running = true;
-		}
-		synchronized(instances) {
-			for(LinkManager manager : instances.values()) {
-				dbQuery db = dbQuery.get(manager.dbKey);
-				if(db == null) {
-					System.out.println("Failed to find db connection.");
-					continue;
-				}
-				try {
-					// query Actions table (inbound updates)
-					manager.triggerActions(db);
-					// update cached data (outbound updates)
-					manager.triggerUpdates();
-				} finally {
-					db.release();
-				}
-			}
-		}
-		running = false;
-	}
-
-
-	// query Actions table (inbound updates)
-	private void triggerActions(dbQuery db) {
-		// lock table
-		db.prepare("LOCK TABLES `"+getTableName()+"` WRITE /* lock actions table */");
-		if(!db.exec()) return;
-		// query table
-		db.prepare("SELECT `action_id`, `player`, `handler`, `action` FROM `"+getTableName()+"` WHERE TRUE ORDER BY `priority` DESC LIMIT 100 /* query actions table */");
-		if(!db.exec()) return;
-		if(db.getResultInt() == 0) return;
-		List<Integer> deleteList = new ArrayList<Integer>();
+	// outbound updates to db
+	private void triggerUpdates() {
 		synchronized(handlers) {
+			for(ActionHandler handler : handlers.values())
+				handler.doUpdate();
+		}
+	}
+
+
+	// inbound updates
+	private void triggerActions() {
+		// query Actions table
+		dbQuery db = dbQuery.get(dbKey);
+		try {
+			// lock table
+			db.prepare("LOCK TABLES `"+getTableName()+"` WRITE /* lock actions table */");
+			if(!db.exec()) return;
+
+
+			// query table
+			db.prepare("SELECT `action_id`, `player`, `handler`, `action` FROM `"+getTableName()+"` "+
+				"WHERE TRUE ORDER BY `priority` DESC LIMIT 100 /* query actions table */");
+			if(!db.exec()) return;
+			PluginManager pm = Bukkit.getServer().getPluginManager();
+			List<Integer> deleteList = new ArrayList<Integer>();
 			while(db.hasNext()) {
 				String handlerName = db.getString("handler");
-				if(handlerName != null && !handlerName.isEmpty()) {
-					linkHandler handler = getHandler(handlerName);
-					if(handler == null) {
-						System.out.println("Handler not found for action: ["+db.getString("handler")+"] "+db.getString("action"));
-					} else {
-						// send action to handler
-						new actionRunnable(
-							handler,
-							db.getString("player"),
-							db.getString("action")
-						).runTask(WebAPI.get());
-					}
+				if(handlerName == null || handlerName.isEmpty()) {
+					// removal list
+					deleteList.add(db.getInt("action_id"));
+					db.clean();
+					continue;
 				}
+				// trigger event
+				ActionEvent event = new ActionEvent(
+					dbKey,
+					db.getString("player"),
+					db.getString("handler"),
+					db.getString("action")
+				);
+				// call event from main thread
+				(new ActionTask(event))
+					.runTask(WebAPI.get());
+				pm.callEvent(event);
+				// removal list
 				deleteList.add(db.getInt("action_id"));
 				db.clean();
 			}
-		}
-		// remove completed actions
-		int deleteSize = deleteList.size();
-		if(deleteSize > 0) {
-			db.prepare("DELETE FROM `"+getTableName()+"` WHERE "+utilsString.repeat(" OR ", "`action_id` = ?", deleteSize)+" LIMIT "+Integer.toString(deleteSize));
-			{
-				int i = 0;
-				for(int id : deleteList) {
-					i++;
-					db.setInt(i, id);
-				}
+			// remove completed actions
+			int deleteSize = deleteList.size();
+			if(deleteSize > 0) {
+//				db.prepare("DELETE FROM `"+getTableName()+"` WHERE "+utilsString.repeat(" OR ", "`action_id` = ?", deleteSize)+" LIMIT "+Integer.toString(deleteSize));
+//				{
+//					int i = 0;
+//					for(int id : deleteList) {
+//						i++;
+//						db.setInt(i, id);
+//					}
+//				}
+//				db.exec();
+				System.out.println("Triggered [ "+Integer.toString(deleteSize)+" ] actions.");
 			}
+			db.clean();
+
+
+
+		} finally {
+			// unlock table
+			db.prepare("UNLOCK TABLES /* unlock actions table */");
 			db.exec();
-			System.out.println("Triggered [ "+Integer.toString(deleteSize)+" ] actions.");
-		}
-		// unlock table
-		db.prepare("UNLOCK TABLES /* unlock actions table */");
-		db.exec();
-		db.clean();
-	}
-	// update cached data (outbound updates)
-	private void triggerUpdates() {
-		synchronized(handlers) {
-			for(linkHandler handler : handlers.values())
-				handler.doUpdate();
+			db.release();
+System.exit(0);
 		}
 	}
 
