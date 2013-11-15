ced935002534c5ac9deba99ca51eda1ffa76bf7b
Pete Lorenzo <lorenzo@poixson.com>
2013-11-15 17:25:38 -0500

    changed linkHandler abstract class to ActionHandler interface

--- java/src/com/poixson/webxbukkit/webLink/ActionHandler.java
+++ java/src/com/poixson/webxbukkit/webLink/ActionHandler.java
@@ -1,28 +1,16 @@
 package com.poixson.webxbukkit.webLink;
 
-
-public abstract class linkHandler {
-
-	private final String handlerName;
-	protected final String dbKey;
+import org.bukkit.event.Listener;
 
 
-	public linkHandler(String handlerName, String dbKey) {
-		if(handlerName == null || handlerName.isEmpty()) throw new NullPointerException("handlerName cannot be null");
-		if(dbKey       == null || dbKey.isEmpty()      ) throw new NullPointerException("dbKey cannot be null");
-		this.handlerName = handlerName;
-		this.dbKey = dbKey;
-	}
+public interface ActionHandler extends Listener {
+
 	// handler name
-	public String getHandlerName() {
-		return handlerName;
-	}
-
+	public String getHandlerName();
 
 	// outbound updates
-	public abstract void doUpdate();
+	public void doUpdate();
 	// inbound updates
-	public abstract void doAction(String player, String action);
-
+	public void onAction(ActionEvent event);
 
 }
