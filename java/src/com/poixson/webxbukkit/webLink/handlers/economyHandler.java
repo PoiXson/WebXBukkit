ff1d7a714b8214a80c2a330fb01f33493d8e1d5c
Pete Lorenzo <lorenzo@poixson.com>
2013-11-17 12:40:44 -0500

    finished economyHandler class to update db and listen for actions

--- java/src/com/poixson/webxbukkit/webLink/handlers/economyHandler.java
+++ java/src/com/poixson/webxbukkit/webLink/handlers/economyHandler.java
@@ -1,7 +1,19 @@
 package com.poixson.webxbukkit.webLink.handlers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import net.milkbowl.vault.economy.Economy;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 
+import com.poixson.commonjava.Utils.StringParser;
+import com.poixson.commonjava.Utils.utilsMath;
+import com.poixson.commonjava.pxdb.dbQuery;
+import com.poixson.webxbukkit.BukkitThreadSafe;
+import com.poixson.webxbukkit.Plugins3rdParty;
+import com.poixson.webxbukkit.WebAPI;
 import com.poixson.webxbukkit.webLink.ActionEvent;
 import com.poixson.webxbukkit.webLink.ActionHandler;
 
@@ -10,6 +22,8 @@ public class economyHandler implements ActionHandler {
 
 	public static final String HANDLER_NAME = "economy";
 
+	private final Map<String, Double> cachedMoney = new HashMap<String, Double>();
+
 
 	@Override
 	public String getHandlerName() {
@@ -17,8 +31,46 @@ public class economyHandler implements ActionHandler {
 	}
 
 
+	private final Object updateLock = new Object();
 	@Override
 	public void doUpdate(String dbKey) {
+		Economy econ = Plugins3rdParty.get().getEconomy();
+		synchronized(updateLock) {
+			// get online players
+			Player[] players = BukkitThreadSafe.getOnlinePlayers();
+			if(players.length == 0) return;
+			dbQuery db = dbQuery.get(dbKey);
+			try {
+				for(Player p : players) {
+					if(p == null) continue;
+					String playerName = p.getName();
+					Double money = econ.getBalance(playerName);
+					if(money == null) continue;
+					Double cached = cachedMoney.get(playerName);
+					// balance hasn't changed
+					if(cached != null && money == cached) continue;
+					// update db cache
+					db.prepare("UPDATE `pxn_Players` SET `money` = ? WHERE `name` = ? LIMIT 1");
+					db.setDecimal(1, money);
+					db.setString (2, playerName);
+					db.exec();
+					if(db.getAffectedRows() == 0) {
+						// insert new row
+						db.clean();
+						db.prepare("INSERT INTO `pxn_Players` (`name`, `money`) VALUES (?, ?)");
+						db.setString (1, playerName);
+						db.setDecimal(2, money);
+						db.exec();
+						if(db.getAffectedRows() == 0)
+							System.out.println("Failed to create new player account");
+					}
+					db.clean();
+					cachedMoney.put(playerName, money);
+				}
+			} finally {
+				db.release();
+			}
+		}
 	}
 
 
@@ -26,10 +78,50 @@ public class economyHandler implements ActionHandler {
 	@EventHandler
 	public void onAction(ActionEvent event) {
 		if(event.isCancelled()) return;
-		if(!event.equalsHandler(HANDLER_NAME)) return;
+		if(!event.isHandler(HANDLER_NAME)) return;
+		// action parser
+		StringParser action = event.getActionParser();
+		if(action == null || !action.next()) return;
 
-System.out.println("HANDLER: "+event.getHandlerName());
-System.out.println("ACTION: "+event.getActionName());
+		// deposit
+		if(action.isFirst("deposit")) {
+			// player
+			final String playerName = event.getPlayerName();
+			// amount
+			final Double amount = utilsMath.parseDouble(action.getNext());
+			if(amount == null) {
+				System.out.println("Invalid deposit amount "+action.get());
+				return;
+			}
+			// call vault api
+			WebAPI.get().getEconomy().depositPlayer(
+				playerName,
+				amount
+			);
+			System.out.println("Deposit "+Double.toString(amount)+" to "+playerName);
+		} else
+
+		// withdraw
+		if(action.isFirst("withdraw")) {
+			// player
+			final String playerName = event.getPlayerName();
+			// amount
+			final Double amount = utilsMath.parseDouble(action.getNext());
+			if(amount == null) {
+				System.out.println("Invalid withdraw amount "+action.get());
+				return;
+			}
+			// call vault api
+			WebAPI.get().getEconomy().withdrawPlayer(
+				playerName,
+				amount
+			);
+			System.out.println("Withdraw "+Double.toString(amount)+" from "+playerName);
+
+		// unknown action
+		} else {
+			System.out.println("Failed to execute action: "+event.toString());
+		}
 	}
 
 
