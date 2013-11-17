56a43ef18898136ad794fd7f136cfb0407701fc4
Pete Lorenzo <lorenzo@poixson.com>
2013-11-17 11:45:55 -0500

    added getters getPlayer() getPlayerName() getActionStr() and getActionParser() to ActionEvent class also added isPlayer() and isHandler()

--- java/src/com/poixson/webxbukkit/webLink/ActionEvent.java
+++ java/src/com/poixson/webxbukkit/webLink/ActionEvent.java
@@ -1,9 +1,12 @@
 package com.poixson.webxbukkit.webLink;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+import com.poixson.commonjava.Utils.StringParser;
 import com.poixson.commonjava.pxdb.dbQuery;
 
 
@@ -35,30 +38,47 @@ public class ActionEvent extends Event implements Cancellable {
 	}
 
 
+	// getters
 	public String dbKey() {
 		return this.dbKey;
 	}
 	public dbQuery getDB() {
 		return dbQuery.get(dbKey());
 	}
-	public String getPlayer() {
+	public String getPlayerName() {
 		return player;
+	}
+	public Player getPlayer() {
+		return Bukkit.getPlayer(getPlayerName());
 	}
 	public String getHandlerName() {
 		return handlerName;
 	}
-	public String getActionName() {
+	public String getActionStr() {
 		return actionName;
 	}
-
-
-	public boolean equalsHandler(String str) {
-		if(str == null || str.isEmpty())
-			return false;
-		return (str.equalsIgnoreCase(handlerName));
+	public StringParser getActionParser() {
+		return new StringParser(" ", getActionStr());
 	}
 
 
+	// player equals
+	public boolean isPlayer(String playerName) {
+		if(playerName == null || playerName.isEmpty()) return false;
+		return playerName.equalsIgnoreCase(this.player);
+	}
+	public boolean isPlayer(Player player) {
+		if(player == null) return false;
+		return isPlayer(player.getName());
+	}
+	// handler equals
+	public boolean isHandler(String str) {
+		if(str == null || str.isEmpty()) return false;
+		return str.equalsIgnoreCase(handlerName);
+	}
+
+
+	// event cancelled
 	@Override
 	public boolean isCancelled() {
 		return cancelled;
