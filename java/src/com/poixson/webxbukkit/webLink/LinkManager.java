5234a39bf43a6277fe87a227e683bb9021e16c2e
Pete Lorenzo <lorenzo@poixson.com>
2013-12-03 13:03:00 -0500

    finished reworking of web link classes threaded scheduler reads Actions table and creates event objects events fire in main thread and remove Actions table entry before performing action (prevents concurrency issues) scheduler -> updateManagers() -> doUpdates() -> execActions() and execUpdates()

--- java/src/com/poixson/webxbukkit/webLink/LinkManager.java
+++ java/src/com/poixson/webxbukkit/webLink/LinkManager.java
@@ -5,13 +5,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.bukkit.Bukkit;
-import org.bukkit.scheduler.BukkitRunnable;
-
-import com.poixson.commonjava.Utils.utilsString;
+import com.poixson.commonjava.Utils.CoolDown;
+import com.poixson.commonjava.Utils.utilsMath;
 import com.poixson.commonjava.Utils.xTime;
 import com.poixson.commonjava.pxdb.dbQuery;
+import com.poixson.webxbukkit.SafetyBukkit;
 import com.poixson.webxbukkit.WebAPI;
+import com.poixson.webxbukkit.xBukkitRunnable;
 import com.poixson.webxbukkit.webLink.handlers.economyHandler;
 import com.poixson.webxbukkit.webLink.handlers.inventoryHandler;
 import com.poixson.webxbukkit.webLink.handlers.permsHandler;
@@ -24,17 +24,33 @@ public class LinkManager {
 		throw new CloneNotSupportedException();
 	}
 
+	// actions table
+	private static final String TABLE_ACTIONS = "pxn_Actions";
+
 	// manager instances
 	private static final Map<String, LinkManager> managers  = new HashMap<String, LinkManager>();
 	// handler instances
 	private final Map<String, ActionHandler> handlers = new HashMap<String, ActionHandler>();
-
 	// db connection key
 	private final String dbKey;
 
+	// update interval
+	private static final xTime intervalShort = xTime.get("0.5s");
+	private static final xTime intervalIdle  = xTime.get("5s");
+	private static final CoolDown idle = CoolDown.get(intervalIdle);
+	private static final int ActionsPerCycle = 5;
+	private static volatile boolean hasMore = false;
+	private volatile int lastId = 0;
+
 	// update task
-	private static volatile UpdateTask task = null;
-	private static volatile Boolean active = false;
+	private static final xBukkitRunnable task = new xBukkitRunnable("WebLinkUpdate", false) {
+		@Override
+		public void runTask() {
+			updateManagers();
+		}
+	};
+	private static volatile Boolean running = false;
+	private static volatile boolean stopping = false;
 
 
 	// get manager instance (per db key)
@@ -53,11 +69,164 @@ public class LinkManager {
 	private LinkManager(String dbKey) {
 		if(dbKey == null || dbKey.isEmpty()) throw new NullPointerException("dbKey cannot be null");
 		this.dbKey = dbKey;
-		// register default handlers
+		// register default handlers (disabled by default)
 		register(new economyHandler   (dbKey));
 		register(new inventoryHandler (dbKey));
 		register(new permsHandler     (dbKey));
 		register(new worldguardHandler(dbKey));
+		// start task
+		start();
+	}
+
+
+	// action/update task
+	private static void start() {
+		if(running == null) running = false;
+		if(running == true) return;
+		synchronized(running) {
+			if(running) return;
+			stopping = false;
+			task.runTaskTimerAsynchronously(
+				WebAPI.get(),
+				utilsMath.MinMax(
+					intervalShort.getTicks() * 2,
+					20 * 3,
+					20 * 15
+				),
+				intervalShort.getTicks()
+			);
+		}
+	}
+	public static void shutdown() {
+		stopping = true;
+		try {
+			task.cancel();
+		} catch (Exception ignore) {}
+	}
+
+
+	// updates task
+	private static void updateManagers() {
+		if(stopping) return;
+		if(!hasMore && !idle.runAgain()) return;
+		hasMore = false;
+		synchronized(managers) {
+			for(LinkManager manager : managers.values()) {
+				try {
+					manager.doUpdates();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		if(hasMore) {
+			idle.reset();
+			idle.runAgain();
+		}
+	}
+	private void doUpdates() {
+		if(stopping) return;
+		dbQuery db = dbQuery.get(dbKey);
+		if(stopping) return;
+		try {
+			// query actions (inbound)
+			execActions(db);
+			if(stopping) return;
+			// update db cache (outbound)
+			execUpdates();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			db.release();
+		}
+	}
+	// query actions (inbound)
+	private void execActions(dbQuery db) {
+		if(stopping) return;
+		if(db == null) return;
+		// query actions
+		ActionEvent[] actions = queryActions(db);
+		if(actions == null) {
+			System.out.println("Failed to query actions!");
+			return;
+		}
+		if(actions.length == 0)
+			return;
+		// not idling
+		hasMore = true;
+		// call events
+		int count = 0;
+		for(ActionEvent event : actions) {
+			if(stopping) break;
+			// call in main thread
+			SafetyBukkit.callEvent(event);
+			count++;
+		}
+		System.out.println("Trigger [ "+Integer.toString(count)+" ] actions..");
+	}
+	// update db cache (outbound)
+	private void execUpdates() {
+		if(stopping) return;
+		synchronized(handlers) {
+			for(ActionHandler handler : handlers.values()) {
+				if(stopping) break;
+				if(handler == null) continue;
+				if(handler.isEnabled())
+					handler.doUpdate();
+			}
+		}
+	}
+
+
+	// query actions table (inbound updates)
+	private ActionEvent[] queryActions(dbQuery db) {
+		if(stopping) return null;
+		if(db == null) return null;
+		// query table
+		db.prepare("SELECT `action_id`, `player`, `handler`, `action` "+
+			"FROM `"+TABLE_ACTIONS+"` "+
+			"WHERE `action_id` > ? "+
+			"ORDER BY `priority` DESC, `action_id` ASC "+
+			"LIMIT "+Integer.toString(ActionsPerCycle)+
+			" /* query actions table */");
+		db.setInt(1, lastId);
+		if(!db.exec())
+			return null;
+		// iterate actions
+		List<ActionEvent> actions = new ArrayList<ActionEvent>();
+		while(db.next()) {
+			String handlerName = db.getString("handler");
+			if(handlerName != null && !handlerName.isEmpty()) {
+				// action event
+				actions.add(
+					new ActionEvent(
+						db.dbKey(),
+						db.getInt("action_id"),
+						db.getString("player"),
+						db.getString("handler"),
+						db.getString("action")
+					)
+				);
+				lastId = db.getInt("action_id");
+			}
+		}
+		db.clean();
+		// action events to perform
+		return actions.toArray(new ActionEvent[actions.size()]);
+	}
+	// remove completed action from db
+	public static boolean removeAction(String dbKey, int id) {
+		if(stopping) return false;
+		dbQuery db = dbQuery.get(dbKey);
+		try {
+			db.prepare("DELETE FROM `"+TABLE_ACTIONS+"` WHERE `action_id` = ? LIMIT 1");
+			db.setInt(1, id);
+			if(!db.exec())
+				return false;
+			return (db.getAffectedRows() != 0);
+		} finally {
+			db.release();
+		}
 	}
 
 
@@ -88,148 +257,13 @@ public class LinkManager {
 		}
 		handler.setEnabled(enabled);
 	}
-
-
-	// start task
-	public void start() {
-		if(task != null) return;
-		synchronized(active) {
-			if(task != null) return;
-			task = new UpdateTask(this);
-			task.runTaskTimerAsynchronously(
-				WebAPI.get(),
-				xTime.get("6s").getTicks(),
-				xTime.get("3s").getTicks()
-			);
+	public Boolean getEnabled(String handlerName) {
+		ActionHandler handler = getHandler(handlerName);
+		if(handler == null) {
+			System.out.println("Unknown web action handler: "+handlerName);
+			return null;
 		}
-	}
-
-
-	// update task
-	private class UpdateTask extends BukkitRunnable {
-
-		private final LinkManager manager;
-		public UpdateTask(LinkManager manager) {
-			super();
-			this.manager = manager;
-		}
-
-		@Override
-		public void run() {
-			synchronized(active) {
-				if(active) {
-					System.out.println("WebLink update task still running..");
-					return;
-				}
-				active = true;
-			}
-			String threadName = Thread.currentThread().getName();
-			Thread.currentThread().setName("WebLinkUpdate");
-			manager.triggerUpdates();
-			manager.triggerActions();
-			Thread.currentThread().setName(threadName);
-			active = false;
-		}
-
-	}
-	// fire event in main thread
-	private class ActionTask extends BukkitRunnable {
-
-		private final ActionEvent event;
-		public ActionTask(ActionEvent event) {
-			this.event = event;
-		}
-
-		@Override
-		public void run() {
-			Bukkit.getPluginManager().callEvent(event);
-			if(event.isCancelled())
-				System.out.println("Event "+Integer.toString(event.getId())+" was cancelled.");
-			else
-				System.out.println("Event "+Integer.toString(event.getId())+" was called.");
-		}
-
-	}
-
-
-	// outbound updates to db
-	private void triggerUpdates() {
-		synchronized(handlers) {
-			for(ActionHandler handler : handlers.values())
-				if(handler.isEnabled())
-					handler.doUpdate();
-		}
-	}
-
-
-	// inbound updates
-	private void triggerActions() {
-		// query Actions table
-		dbQuery db = dbQuery.get(dbKey);
-		try {
-			// lock table
-			db.prepare("LOCK TABLES `"+getTableName()+"` WRITE /* lock actions table */");
-			if(!db.exec()) return;
-			// query table
-			db.prepare("SELECT `action_id`, `player`, `handler`, `action` FROM `"+getTableName()+"` "+
-				"WHERE TRUE ORDER BY `priority` DESC, `action_id` ASC LIMIT 100 /* query actions table */");
-			if(!db.exec()) return;
-			List<Integer> deleteList = new ArrayList<Integer>();
-
-			// iterate actions
-			while(db.next()) {
-				String handlerName = db.getString("handler");
-				if(handlerName == null || handlerName.isEmpty()) {
-					// removal list
-					deleteList.add(db.getInt("action_id"));
-					continue;
-				}
-				// trigger event
-				ActionEvent event = new ActionEvent(
-					dbKey,
-					db.getInt("action_id"),
-					db.getString("player"),
-					db.getString("handler"),
-					db.getString("action")
-				);
-				{
-					// call event from main thread
-					ActionTask task = new ActionTask(event);
-					task.runTask(WebAPI.get());
-				}
-				// removal list
-				deleteList.add(db.getInt("action_id"));
-			}
-			db.clean();
-
-			// remove completed actions
-			int deleteSize = deleteList.size();
-			if(deleteSize > 0) {
-				db.prepare("DELETE FROM `"+getTableName()+"` WHERE "+utilsString.repeat(" OR ", "`action_id` = ?", deleteSize)+" LIMIT "+Integer.toString(deleteSize));
-				{
-					int i = 0;
-					for(int id : deleteList) {
-						i++;
-						db.setInt(i, id);
-					}
-					deleteList.clear();
-				}
-				db.exec();
-				System.out.println("Triggered [ "+Integer.toString(deleteSize)+" ] actions.");
-			}
-			db.clean();
-
-		} finally {
-			// unlock table
-			db.prepare("UNLOCK TABLES /* unlock actions table */");
-			db.exec();
-			db.release();
-		}
-	}
-
-
-	private String getTableName() {
-		return dbQuery.san("pxn_Actions");
+		return handler.isEnabled();
 	}
 
 
