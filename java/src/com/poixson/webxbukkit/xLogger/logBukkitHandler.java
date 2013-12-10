1a7fa7b6c463ff8b7dce9f779d48c7e245229fae
Pete Lorenzo <lorenzo@poixson.com>
2013-12-10 14:58:42 -0500

    added _getBukkitLogger() and _getBukkitHandlers() to logBukkitHandler and updated publish() override

--- java/src/com/poixson/webxbukkit/xLogger/logBukkitHandler.java
+++ java/src/com/poixson/webxbukkit/xLogger/logBukkitHandler.java
@@ -1,27 +1,39 @@
 package com.poixson.webxbukkit.xLogger;
 
-import java.util.logging.Logger;
-
+import com.poixson.commonjava.xVars;
+import com.poixson.commonjava.xLogger.xLevel;
 import com.poixson.commonjava.xLogger.xLogRecord;
 import com.poixson.commonjava.xLogger.handlers.logHandlerConsole;
 
 
 public class logBukkitHandler extends logHandlerConsole {
 
-	// bukkit logger
-	private final Logger bukkitLog;
 
-
+	// get bukkit logger
+	private final java.util.logging.Logger bukkitLog;
 	public logBukkitHandler() {
-		bukkitLog = Logger.getGlobal();
+		bukkitLog = java.util.logging.Logger.getGlobal();
+		// set bukkit log level (if needed)
+		setLevel(getLevel());
+	}
+	private java.util.logging.Logger _getBukkitLogger() {
+		return bukkitLog;
+	}
+	// bukkit log handlers
+	private java.util.logging.Handler[] _getBukkitHandlers() {
+		try {
+			return _getBukkitLogger().getParent().getHandlers();
+		} catch (Exception ignore) {}
+		return null;
 	}
 
 
+	// publish to bukkit
 	@Override
 	public void publish(xLogRecord record) {
-		bukkitLog.log(
+		_getBukkitLogger().log(
 			record.getJavaLevel(),
-			doFormat(record)
+			msgFormat(record)
 		);
 	}
 
